f9ad8f7d6c00174d63e6a044101fb5aca6c8cb3b
FADL285 <53804570+FADL285@users.noreply.github.com>
2022-12-15 13:39:08 +0200

    chore: Add FormKit Config 🚀

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@formkit/nuxt'],
+  formkit: {
+    configFile: './my-configs/formkit.config.ts',
+  },
 });
